c98bdb63dfe6bb9bd97595801572e72a58c6340c
Sidclei-Silvio <sidclei.silvio.santos@gmail.com>
2023-07-24 20:44:47 -0300

    Feito uma correção no código do app: calculo_hora_trabalhada

--- python/Python_objeto/calculo_hora_trabalhada.py
+++ python/Python_objeto/calculo_hora_trabalhada.py
@@ -10,15 +10,18 @@ class Funcionario():
     def salario(self):
         return self.__salario
     
-    def registra_hora_trabalhada(self):
-        self.__horas_trabalhadas += 1
+    def registra_hora_trabalhada(self, horas):
+        self.__horas_trabalhadas += horas
         
     def calcula_salario(self):
         self.__salario = self.__horas_trabalhadas * self.valor_hora_trabalhada
         return self.__salario
 
 jose = Funcionario('José', 'Professor', 50)
-jose.registra_hora_trabalhada()
-jose.registra_hora_trabalhada()
-jose.registra_hora_trabalhada()
-print(jose.calcula_salario())  # O salário será calculado com base nas horas trabalhadas (3) e no valor da hora (50).
+
+# Vamos supor que José trabalhou 8 horas em um dia e 6 horas em outro dia.
+jose.registra_hora_trabalhada(8)
+jose.registra_hora_trabalhada(6)
+
+# Agora, calculamos o salário com base nas horas trabalhadas registradas.
+print(jose.calcula_salario())  # O salário será calculado com base nas horas trabalhadas (8 + 6) e no valor da hora (50).
